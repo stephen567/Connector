58b0d4d02e63fd0461fa81951990c9058055e371
stephen567 <Stephen@DESKTOP-1DL9ASC>
2018-08-06 14:13:09 +0800

    中间结果

---
+++ src/main/java/com/syscom/banksys/connector/MQSender.java
@@ -0,0 +1,46 @@
+package com.syscom.banksys.connector;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MQSender 
+{
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private String QUEUE_NAME;
+	private Channel MQChannel;
+	private Connection connection;
+	
+	public MQSender(String host) throws Exception
+	{
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(host);
+		connection = factory.newConnection();
+		MQChannel = connection.createChannel();
+		
+		MQChannel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		
+	}
+	
+	public void SendMsg(Byte[] msg) throws Exception
+	{
+		String message = msg.toString();
+		
+		
+		MQChannel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+		logger.debug("Send the Message " + message + " to RabbitMQ.");
+	}
+	
+	public void closeMQ() throws Exception
+	{
+		logger.info("¹Ø±ÕRabbitMQÍ¨µÀ£¡");
+		
+		MQChannel.close();
+		connection.close();
+	}
+
+}

59ece360df0ec5067f01650a5242bad6c1d3b5ab
stephen567 <Stephen@DESKTOP-1DL9ASC>
2018-08-29 14:59:06 +0800

    commit on 0829 15:00

--- src/main/java/cn/com/syscom/banksys/connector/handler/MsgInboundHandler.java
+++ src/main/java/cn/com/syscom/banksys/connector/handler/MsgInboundHandler.java
@@ -1,28 +1,23 @@
-package cn.com.syscom.banksys.connector.client;
+package cn.com.syscom.banksys.connector.handler;
 
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import cn.com.syscom.banksys.connector.handler.Heartbeats;
-import cn.com.syscom.banksys.connector.mq.ConnectorMQSender;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
-import java.text.SimpleDateFormat;
+import cn.com.syscom.banksys.connector.mq.ConnectorMQSender;
 
 @ChannelHandler.Sharable
-public class MsgClientHandler extends SimpleChannelInboundHandler 
+public class MsgInboundHandler extends SimpleChannelInboundHandler<Object> 
 {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	/*
+/*
 	private final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 	private String FormattedDate()
 	{
@@ -30,20 +25,30 @@ public class MsgClientHandler extends SimpleChannelInboundHandler
 		
 		return df.format(System.currentTimeMillis());
 	}
-	*/
+*/
 	
 	private Channel channel;
+	private ConnectorMQSender MQSender;
 
-	public MsgClientHandler()
+	public MsgInboundHandler()
 	{
-		logger.debug("通讯客户端处理进程MsgClientHandler初始化");
+		logger.info("通讯客户端处理进程MsgClientHandler初始化");
+		
+		MQSender = new ConnectorMQSender("Producer.xml");
 	}
-	
+
+	public MsgInboundHandler(String MQSendXMLConfig)
+	{
+		logger.info("通讯客户端处理进程MsgClientHandler初始化");
+		
+		MQSender = new ConnectorMQSender(MQSendXMLConfig);
+	}
+
 	public void channelActive(ChannelHandlerContext ctx)
 	{
 		this.channel = ctx.channel();
 		
-		ctx.fireChannelInactive();
+		ctx.fireChannelActive();
 	}
 	
 	@Override
@@ -60,33 +65,22 @@ public class MsgClientHandler extends SimpleChannelInboundHandler
 		buf.readBytes(bytes);
 		
 		String message = bytestoHexString(bytes);
-		logger.debug("通讯客户端读取到报文内容为： {}", message);
+		logger.info("通讯客户端读取到报文内容为： {}", message);
+		
+		//logger.info("通讯客户端读取到报文内容为： {}", bytes.toString());
 
 		//ApplicationContext context = new ClassPathXmlApplicationContext("Producer.xml");
 		//RabbitTemplate amqptemplate = context.getBean(RabbitTemplate.class);
 		
 		//amqptemplate.convertAndSend(message);
-		ConnectorMQSender MQSender = new ConnectorMQSender("Producer.xml");
+		
+		//ConnectorMQSender MQSender = new ConnectorMQSender("Producer.xml");
 		MQSender.convertAndSend(message);
-	}
+		//MQSender.convertAndSend(buf);
 
-    @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception 
-    {
-        if (evt instanceof IdleStateEvent) 
-        {
-        	IdleState state = ((IdleStateEvent) evt).state();
-        	
-            if (state == IdleState.WRITER_IDLE) 
-            {
-            	logger.info("发送写空闲心跳包！");
-                ctx.writeAndFlush(Heartbeats.heartbeatContent());
-            }
-        } else 
-        {
-            super.userEventTriggered(ctx, evt);
-        }
-    }
+		//String respMsg = "16010200ac00000000000000000000000000000000000810822000000800020204000000000000000822071751000223f8f2f3f4f0f7f0f0f0f2f2f3670100649f3303204000950580000100009f37049badbcab9f100c0b010a03a0b00000000000009f26080123456789abcdef9f360200ff820200009c01009f1a0208409a030101019f02060000000123005f2a0208409f03060000000000008407a00000000310100580000000020071";
+		//ctx.writeAndFlush(respMsg);
+	}
 
 	public String bytestoHexString(byte[] src)
 	{
@@ -99,7 +93,7 @@ public class MsgClientHandler extends SimpleChannelInboundHandler
 		for(int i = 0; i < src.length; i++)
 		{
 			int v = src[i] & 0xFF;
-			String hv = Integer.toHexString(v);
+			String hv = Integer.toHexString(v).toUpperCase();
 			if (hv.length() < 2)
 			{
 				stringBuilder.append(0);
@@ -115,7 +109,4 @@ public class MsgClientHandler extends SimpleChannelInboundHandler
 		logger.error("读取交易报文异常退出：{} ", cause.getMessage());
 		ctx.close();
 	}
-	
-	
-
 }
